f340403e5dfeed4d4833397ad1a95af59ea92296
miyake <fumiyoshi_miyake@sobal.co.jp>
2020-08-13 16:29:12 +0900

    Add Sim Input

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import time
+import module
+import setting
+
+
 
 try:
      while(True):
@@ -6,7 +10,13 @@ try:
         time.sleep(.1)
         #時間表示
         print(time.time())
+        temp = module.readTemp()
+        if setting.debug:
+            print(temp)
 
+        pic = module.readPic()
+        if setting.debug:
+            print(pic)
 #’Ctrl+C’を受け付けると終了
 except KeyboardInterrupt:
     print("done")
