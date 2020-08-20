0b152936bc0e650172db15cbbd6eb1e2eb43df54
yuta0627 <yuuta_shimada@sobal.co.jp>
2020-08-20 13:19:41 +0900

    温度データ二次元配列読みとり

--- module.py
+++ module.py
@@ -12,7 +12,6 @@ if setting.mode == 1 :
     sensorCount = 64 
     #サーモセンサーのデータ配列
     temperature = [[0]*8]*8
-    #temperature = [0] * sensorCount
 
     #シュミレート用の画像データ 
     pic_list = ["picture0.jpg", "picture1.jpg", "picture2.jpg", "picture3.jpg", "picture4.jpg", "picture5.jpg", "picture6.jpg", "picture7.jpg", "picture8.jpg", "picture9.jpg"]
@@ -27,11 +26,11 @@ if setting.mode == 1 :
             with open(sensor_file) as f:
                 sensorData = [row for row in csv.reader(f)]
         
-            #特定の列の64行分のデータを取得
+            #8×8のデータを取得
             for row in range(8):
                 temperature[row] = sensorData[snsor_data_count * 8 + row]
 
-            #10個読み込んだ場合は先頭に戻す
+            #8個読み込んだ場合は先頭に戻す
             if snsor_data_count == 7:
                 snsor_data_count = 0
             else:
